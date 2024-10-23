7ec30505b72124100b7cb6fe07394683b7eb80a2
adleomalik <adleomalik@gmail.com>
2024-10-24 00:49:55 +0700

    progress perencanaan lvl 3

---
+++ src/pages/Perencanaan/PerencanaanDetailDaerah/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContentPerencanaanDetailDaerah from './ContentPerencanaanDetailDaerah'
+
+const PerencanaanDetailDaerah = () => {
+  return (
+    <React.Fragment>
+      <div className='page-content'>
+          <ContentPerencanaanDetailDaerah />
+      </div>  
+    </React.Fragment>
+  )
+}
+
+export default PerencanaanDetailDaerah
