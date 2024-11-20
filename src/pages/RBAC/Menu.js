105cf216b17a6484ff67e5e80afcca101bc76557
Saiful Haqqi <saifulhq@gmail.com>
2024-11-20 14:02:47 +0700

    RBAC - form Menu : add pagination, alert for success and fail

--- src/pages/RBAC/Menu.js
+++ src/pages/RBAC/Menu.js
@@ -66,23 +66,75 @@ const Menu = () => {
     const [submitProcess, setSubmitProcess] = useState(false)
     const [modal_center, setmodal_center] = useState(false);
 
+    // pagination
+    const [paging, setPaging] = useState({
+        page: 1,
+        max: 5,
+        size: 10
+    })
+    function page_goto(page) {
+        if (page == paging.page) return;
+        setPaging(Object.assign({}, paging, { page }));
+    }
+    function page_prev() {
+        const { page } = paging;
+        if (page == 1) return;
+        setPaging(Object.assign({}, paging, { page: page - 1 }));
+    }
+    useEffect(() => {
+        console.log('paging', paging);
+        populate_data();
+    }, [paging])
+    function paging_content() {
+        let content = [];
+        for (let i = 0; i < paging.max; i++) {
+            content.push(<div
+                key={'paging-item-' + i}
+                onClick={() => page_goto(i + 1)}
+                className={`page-item d-flex align-items-center justify-content-center ${i + 1 == paging.page ? 'disabled' : ''}`}>
+                {i + 1}</div>)
+        }
+        return content;
+    }
+
+    // modal SUCCESS
+    const [mSuccess, setModalSuccess] = useState({
+        open: false,
+        title: 'Success Message',
+        message: ''
+    })
+    // modal ERROR
+    const [mError, setModalError] = useState({
+        open: false,
+        title: 'Error Message',
+        message: ''
+    })
+
     const onSubmit = async (e) => {
         e.preventDefault();
-        const json = {
-            namaMenu: formData.nama_menu,
-            namaSubMenu: formData.nama_sub_menu,
-            url: formData.url,
-            isMenu: true
-        }
-        let response = api.create(`${API_9007_URI}/rbac/create-menu`, json);
 
-        setSubmitProcess(true);
-        let data = await response;
-        console.log({ data });
-        if (data.code === 200) {
-            populate_data();
+        try {
+            const json = {
+                namaMenu: formData.nama_menu,
+                namaSubMenu: formData.nama_sub_menu,
+                url: formData.url,
+                isMenu: true
+            }
+            let response = api.create(`${API_9007_URI}/rbac/create-menu`, json);
+
+            setSubmitProcess(true);
+            let data = await response;
+            console.log({ data });
+            if (data.code === 200) {
+                populate_data();
+                setShow(false);
+                setModalSuccess(Object.assign({}, mSuccess, { title: "Simpan Data", message: "Proses simpan data berhasil", open: true }))
+            }
+        } catch (error) {
+            setModalError(Object.assign({}, mError, { title: "Error Simpan Data", message: error, open: true }))
+        } finally {
+            setSubmitProcess(false);
         }
-        setSubmitProcess(false);
         return false;
     }
     function changeValue(e) {
@@ -100,16 +152,21 @@ const Menu = () => {
     }, [formData])
 
     useEffect(() => {
-        populate_data();
+        // populate_data();
     }, [])
 
     async function populate_data() {
+        const json = {
+            page: paging.page,
+            size: paging.size
+        }
+        console.log('list data', json);
         let response = api.get(`${API_9007_URI}/rbac/list-menu`);
 
         let data = await response;
 
         if (data.code === 200) {
-            setResultData(data.list.filter(d => d.is_menu));
+            setResultData(data.list); // .filter(d => d.is_menu)
         }
     }
 
@@ -127,6 +184,30 @@ const Menu = () => {
             nama_sub_menu: ""
         })
         setShow(false);
+    }
+
+    async function do_delete() {
+        try {
+            const json = {
+                namaMenu: formData.nama_menu,
+                namaSubMenu: formData.nama_sub_menu,
+                url: formData.url,
+                isMenu: true
+            }
+            let response = api.create(`${API_9007_URI}/rbac/delete-menu`, json);
+
+            setSubmitProcess(true);
+            let data = await response;
+            console.log({ data });
+            if (data.code === 200) {
+                populate_data();
+            }
+        } catch (error) {
+            alert('Error Simpan data');
+        } finally {
+            setSubmitProcess(false);
+        }
+        tog_center();
     }
 
     return (
@@ -203,6 +284,8 @@ const Menu = () => {
                                     fontSize: "12px",
                                     marginBottom: "6px"
                                 }} onClick={() => setShow(true)}>Tambah</button>
+
+                                <div className="paging-container d-flex justify-content-center">{paging_content()}</div>
                                 <table
                                     className="table table-bordered table-nowrap align-middle mb-0"
                                     style={{ width: "100%" }}
@@ -239,19 +322,19 @@ const Menu = () => {
                                                         }}>
                                                     {index + 1}
                                                 </td>
-                                                <td>
+                                                <td style={{ maxWidth: "10%" }}>
                                                     {item.nama_menu}
                                                 </td>
-                                                <td>
+                                                <td style={{ maxWidth: "400px" }} className="text-wrap">
                                                     {item.url}
                                                 </td>
-                                                <td>
+                                                <td style={{ maxWidth: "400px" }} className="text-wrap">
                                                     {item.nama_sub_menu}
                                                 </td>
                                                 <td>
                                                     <Button color="danger" style={{ marginRight: "3px" }} onClick={() => {
-                                                        setShow(false)
-                                                        tog_center()
+                                                        setShow(false);
+                                                        tog_center();
                                                     }}>Hapus</Button>
                                                     <Button color="primary" onClick={() => onEdit(item)}>Ubah</Button>
                                                 </td>
@@ -270,7 +353,7 @@ const Menu = () => {
                     centered
                 >
                     <ModalHeader
-                        className=" p-3 bg-info-subtle" toggle={tog_center}>
+                        className="p-3 bg-info-subtle" toggle={tog_center}>
                         Hapus Data Menu
                     </ModalHeader>
                     <ModalBody>
@@ -281,7 +364,7 @@ const Menu = () => {
                         </Row>
                         <Row>
                             <Col className="d-flex justify-content-center align-items-center" >
-                                <Button color="primary" className="mt-3" style={{ marginRight: "6px" }} onClick={() => tog_center()}>
+                                <Button color="primary" className="mt-3" style={{ marginRight: "6px" }} onClick={() => do_delete()}>
                                     Hapus
                                 </Button>
                                 <Button color="warning" className="mt-3" onClick={() => tog_center()}>
@@ -292,6 +375,55 @@ const Menu = () => {
                     </ModalBody>
                 </Modal>
             </div>
+
+            <Modal
+                isOpen={mError.open}
+                backdrop="static"
+                keyboard={false}
+                toggle={() => setModalError(Object.assign({}, mError, { open: false }))}
+                centered
+            >
+                <div className=" text-center p-5">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/tdrtiskw.json"
+                        trigger="loop"
+                        colors="primary:#f7b84b,secondary:#405189"
+                        style={{ width: "130px", height: "130px" }}>
+                    </lord-icon>
+                    <div className="mt-4 pt-4">
+                        <h4>{mError.title}</h4>
+                        <p className="text-muted"> {mError.message} </p>
+                        <button className="btn btn-warning" onClick={() => setModalError(Object.assign({}, mError, { open: false }))}>
+                            Continue
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+
+            <Modal
+                isOpen={mSuccess.open}
+                backdrop="static"
+                keyboard={false}
+                toggle={() => setModalError(Object.assign({}, mError, { open: false }))}
+                centered
+            >
+                <div className=" text-center p-5">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/lupuorrc.json"
+                        trigger="loop"
+                        colors="primary:#121331,secondary:#08a88a"
+                        style={{ width: "130px", height: "130px" }}>
+                    </lord-icon>
+
+                    <div className="mt-4 pt-4">
+                        <h4>{mSuccess.title}</h4>
+                        <p className="text-muted"> {mSuccess.message} </p>
+                        <button className="btn btn-success" onClick={() => setModalSuccess(Object.assign({}, mSuccess, { open: false }))}>
+                            Complete
+                        </button>
+                    </div>
+                </div>
+            </Modal>
         </>
     );
 }
