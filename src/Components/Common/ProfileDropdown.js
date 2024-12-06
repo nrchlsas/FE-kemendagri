b147d47b594c306535053b2035464a5ca214b441
Saiful Haqqi <saifulhq@gmail.com>
2024-12-06 13:06:52 +0700

    Login first name : default field

--- src/Components/Common/ProfileDropdown.js
+++ src/Components/Common/ProfileDropdown.js
@@ -21,9 +21,10 @@ const ProfileDropdown = () => {
     useEffect(() => {
         const obj = JSON.parse(sessionStorage.getItem("authUser"));
         if (obj) {
-            setUserName(process.env.REACT_APP_DEFAULTAUTH === "fake" ? obj.username === undefined ? user.first_name ? user.first_name : obj.data.first_name : "Admin" || "Admin" :
-                process.env.REACT_APP_DEFAULTAUTH === "firebase" ? obj.providerData[0].email : "Admin"
-            );
+            // setUserName(process.env.REACT_APP_DEFAULTAUTH === "fake" ? obj.username === undefined ? user.first_name ? user.first_name : obj.data.first_name : "Admin" || "Admin" :
+            //     process.env.REACT_APP_DEFAULTAUTH === "firebase" ? obj.providerData[0].email : "Admin"
+            // );
+            setUserName(obj.data.first_name);
         }
     }, [user]);
 
